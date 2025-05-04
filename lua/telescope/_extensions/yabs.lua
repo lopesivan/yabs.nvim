132e5ca1b80d82f04c2cb2d481fcda51801d2c3e
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-05-04 19:34:16 -0300

    [fix] adição de comentario no telescope.

--- lua/telescope/_extensions/yabs.lua
+++ lua/telescope/_extensions/yabs.lua
@@ -9,61 +9,68 @@ local Yabs = require('yabs')
 local scopes = require('yabs.task').scopes
 
 if not Yabs.did_conf then
-  Yabs:load_config_file()
+    Yabs:load_config_file()
 end
 
 local function select_task(opts, scope)
-  opts = themes.get_dropdown(opts)
+    opts = themes.get_dropdown(opts)
 
-  local tasks = Yabs:get_tasks(scope)
-  tasks = vim.tbl_filter(function(task)
-    return not task.disabled
-  end, tasks)
+    local tasks = Yabs:get_tasks(scope)
+    tasks = vim.tbl_filter(function(task)
+        return not task.disabled
+    end, tasks)
 
-  pickers.new(opts, {
-    prompt_title = 'Select a task',
-    finder = finders.new_table({
-      results = tasks,
-      entry_maker = function(entry)
-        local display = entry.name
-        local ordinal = entry.name
-        if type(entry.command) == 'string' then
-          display = string.format('%s: %s', entry.name, entry.command)
-          ordinal = display .. entry.command
-        end
-        return {
-          value = entry.name,
-          display = display,
-          ordinal = ordinal,
-        }
-      end,
-    }),
-    sorter = sorters.get_fzy_sorter(),
-    attach_mappings = function(prompt_bufnr)
-      local source_session = function()
-        actions.close(prompt_bufnr)
-        local entry = actionstate.get_selected_entry(prompt_bufnr)
-        if entry then
-          Yabs:run_task(entry.value, { scope = scope })
-        end
-      end
+    pickers.new(opts, {
+        prompt_title = 'Select a task',
+        finder = finders.new_table({
+            results = tasks,
+            entry_maker = function(entry)
+                local display = entry.name
+                local ordinal = entry.name
+                local d = string.format('%s', entry.name)
+                if entry.desc then
+                    d = string.format('%s: %24s', entry.name, entry.desc)
+                end
+                if type(entry.command) == 'string' then
+                    display = string.format('%: %s', entry.name, entry.command)
+                    if entry.desc then
+                        display = string.format('%: %s %s', entry.name, entry.command, entry.desc)
+                    end
+                    ordinal = display .. entry.command
+                end
+                return {
+                    value = entry.name,
+                    display = d,
+                    ordinal = ordinal,
+                }
+            end,
+        }),
+        sorter = sorters.get_fzy_sorter(),
+        attach_mappings = function(prompt_bufnr)
+            local source_session = function()
+                actions.close(prompt_bufnr)
+                local entry = actionstate.get_selected_entry(prompt_bufnr)
+                if entry then
+                    Yabs:run_task(entry.value, { scope = scope })
+                end
+            end
 
-      actions.select_default:replace(source_session)
-      return true
-    end,
-  }):find()
+            actions.select_default:replace(source_session)
+            return true
+        end,
+    }):find()
 end
 
 return telescope.register_extension({
-  exports = {
-    tasks = function(opts)
-      select_task(opts, scopes.ALL)
-    end,
-    current_language_tasks = function(opts)
-      select_task(opts, scopes.LOCAL)
-    end,
-    global_tasks = function(opts)
-      select_task(opts, scopes.GLOBAL)
-    end,
-  },
+    exports = {
+        tasks = function(opts)
+            select_task(opts, scopes.ALL)
+        end,
+        current_language_tasks = function(opts)
+            select_task(opts, scopes.LOCAL)
+        end,
+        global_tasks = function(opts)
+            select_task(opts, scopes.GLOBAL)
+        end,
+    },
 })
